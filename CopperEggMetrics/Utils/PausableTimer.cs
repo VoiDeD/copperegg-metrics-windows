b6c0ffde908cf51dad67bb228cca1c95835e93fb
Ryan Stecker <voidedweasel@gmail.com>
2013-08-15 21:10:32 -0500

    First attempt at fixing case issue.

---
+++ CopperEggMetrics/Utils/PausableTimer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoppereggMetrics
+{
+    class PausableTimer
+    {
+        Timer timer;
+
+        TimeSpan period;
+        Action<object> func;
+
+
+        public PausableTimer( Action<object> callback, TimeSpan period )
+        {
+            timer = new Timer( OnTick );
+
+            this.period = period;
+            func = callback;
+        }
+
+
+        public void Start()
+        {
+            timer.Change( TimeSpan.Zero, period );
+        }
+        public void Stop()
+        {
+            Pause();
+            timer.Dispose();
+        }
+
+        public void Pause()
+        {
+            timer.Change( Timeout.Infinite, Timeout.Infinite );
+        }
+        public void Resume()
+        {
+            timer.Change( period, period );
+        }
+
+
+        void OnTick( object state )
+        {
+            func( state );
+        }
+    }
+}
