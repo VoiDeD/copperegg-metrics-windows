d3c71f49855ff2a232b874bc78d7ff9f3b1b8c32
Ryan Stecker <voidedweasel@gmail.com>
2013-08-15 21:01:31 -0500

    Add more .NET metrics.

--- CoppereggMetrics/Metrics/Providers/NETMetricProvider.cs
+++ CoppereggMetrics/Metrics/Providers/NETMetricProvider.cs
@@ -19,7 +19,15 @@ namespace CoppereggMetrics
 
         protected override void SetupMetricGroup( MetricGroup metricGroup )
         {
-            SetupMetric( metricGroup, @".NET CLR Exceptions\# of Exceps Thrown\_Global_", "Exceptions Thrown", MetricType.Counter, "Exceptions" );
+            SetupMetric( metricGroup, @".NET CLR Exceptions\# of Exceps Thrown\_Global_", "Exceptions: Exceptions Thrown", MetricType.Counter, "Exceptions" );
+            SetupMetric( metricGroup, @".NET CLR Interop\# of marshalling\_Global_", "Interop: Objects Marshalled", MetricType.Counter, "Objects" );
+            SetupMetric( metricGroup, @".NET CLR Memory\# GC Handles\_Global_", "Memory: # GC Handles", MetricType.Gauge, "GC Handles" );
+            SetupMetric( metricGroup, @".NET CLR Memory\# Total committed Bytes\_Global_", "Memory: Total Committed Bytes", MetricType.Gauge, "Bytes" );
+            SetupMetric( metricGroup, @".NET CLR Memory\Allocated Bytes/sec\_Global_", "Memory: Allocated Bytes/sec", MetricType.Gauge, "Bytes/sec" );
+            SetupMetric( metricGroup, @".NET CLR Memory\Large Object Heap size\_Global_", "Memory: Large Object Heap Size", MetricType.Gauge, "Bytes" );
+            SetupMetric( metricGroup, @".NET CLR Networking\Bytes Received\_global_", "Networking: Bytes Received", MetricType.Counter, "Bytes" );
+            SetupMetric( metricGroup, @".NET CLR Networking\Bytes Sent\_global_", "Networking: Bytes Sent", MetricType.Counter, "Bytes" );
+            SetupMetric( metricGroup, @".NET CLR Networking\Connections Established\_global_", "Networking: Connections Established", MetricType.Counter, "Sockets" );
         }
 
         protected override Task<Dictionary<string, object>> GetMetricSourceData()
@@ -40,14 +48,22 @@ namespace CoppereggMetrics
 
             foreach ( var metric in metricGroup.Metrics )
             {
+                object val = null;
+
                 switch ( metric.Type )
                 {
                     case MetricType.Counter:
-                        sample.Values[ metric.Name ] = Convert.ToInt64( source[ metric.Name ] );
+                    case MetricType.Gauge:
+                        val = Convert.ToInt64( source[ metric.Name ] );
                         break;
 
-                    // todo: more types
+                    // todo: more types?
+                    default:
+                        Debug.Assert( false );
+                        break;
                 }
+
+                sample.Values[ metric.Name ] = val;
             }
 
             return sample;
